8df52a5ea38c6283bde2ec9edc0e0c50c6872817
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-09-06 02:04:31 +0500

    simple converter

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,19 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	EUR      = "EUR"
+	RUB      = "RUB"
+	USD      = "USD"
+	usdToEur = 0.8532
+	// usdToRub = 81.56
+	// eurToRub = 81.56 / usdToEur
+)
 
 func main() {
-	const usdToEur = 0.8532
-	const usdToRub = 81.56
-	const eurToRub = 81.56 / usdToEur
+	source := getSourceCurrency()
+	target := getTargetCurrency(source)
+	q := getMoneyQuantity()
+	result := covertCurrensies(q, source, target)
+	fmt.Printf("%.2f", result)
+
 }
 
-func getUserInput() string {
+func getSourceCurrency() string {
 	var input string
-	fmt.Scan(&input)
+	for {
+		fmt.Println("Введите код валюты, из которой необходимо перевести деньги.")
+		fmt.Println("Доступные валюты: EUR, USD")
+		fmt.Scan(&input)
+
+		if input != EUR && input != USD && input != RUB {
+			continue
+		} else {
+			break
+		}
+	}
+	return input
+
+}
+
+func getTargetCurrency(source string) string {
+	var input string
+	var availableCurrency string
+
+	switch source {
+	case USD:
+		availableCurrency = EUR
+	case EUR:
+		availableCurrency = USD
+	}
+
+	for {
+		fmt.Printf("Введите код валюты, в которую необходимо перевести деньги. Не вводите %s\n", source)
+		fmt.Printf("Доступная валюта: %s\n", availableCurrency)
+		fmt.Scan(&input)
+
+		if input != availableCurrency {
+			continue
+		} else {
+			break
+		}
+	}
+	return input
+
+}
+
+func getMoneyQuantity() float64 {
+	var input float64
+	for {
+		fmt.Println("Сколько хотите перевести? Введите число.")
+		_, err := fmt.Scan(&input)
+
+		if err != nil || input <= 0 {
+			continue
+		} else {
+			break
+		}
+	}
 	return input
 }
 
-func covertCurrensies(n float64, currency1 string, currency2 string) float64 {
-	return 1
+func covertCurrensies(n float64, source string, target string) float64 {
+	var res float64
+	switch {
+	case source == EUR && target == USD:
+		res = n / usdToEur
+	case source == USD && target == EUR:
+		res = n * usdToEur
+	}
+	return res
 }
