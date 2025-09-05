ffd890b682e3091c43e281a7afe2889203f12287
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-09-06 00:44:15 +0500

    functions templates

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	const usdToEur = 0.8532
 	const usdToRub = 81.56
 	const eurToRub = 81.56 / usdToEur
+}
+
+func getUserInput() string {
+	var input string
+	fmt.Scan(&input)
+	return input
+}
+
+func covertCurrensies(n float64, currency1 string, currency2 string) float64 {
+
 }
