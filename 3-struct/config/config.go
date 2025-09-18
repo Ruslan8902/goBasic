f31009088c3f1cd0d1e203a3d5a633ccb36d2697
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-09-18 22:12:27 +0500

    http

--- 3-struct/config/config.go
+++ 3-struct/config/config.go
@@ -1,13 +1,25 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
-	Key string
+	Key     string
+	BaseUrl string
 }
 
 func NewConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	return &Config{
-		Key: os.Getenv("KEY"),
+		Key:     os.Getenv("KEY"),
+		BaseUrl: "https://api.jsonbin.io/v3/b",
 	}
 }
